3bddb5dfa649dc74a2719ba38ab12a1785659864
Your Name <you@example.com>
2024-09-14 16:44:47 +0600

    edit-env

--- index.js
+++ index.js
@@ -15,7 +15,6 @@ app.use(express.json())
 
 
 const uri = "mongodb+srv://blood_server:aq3s5tvBvtDP2znq@cluster0.0b46zlg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
